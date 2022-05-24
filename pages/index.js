0e024ea77bc4135e5fc041ad749a6ea42be1e6a5
AbdiacesMahamed <71865788+AbdiacesMahamed@users.noreply.github.com>
2022-05-24 13:01:16 -0700

    document structure formating
    initial folder commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+      <title>humanbiologics</title>
+        <meta
+          name="description"
+          content="The ecommerce website by next and sanity"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Typography component="h1" variant="h1">
+        humanbiologics
+      </Typography>
+    </div>
+  )}
