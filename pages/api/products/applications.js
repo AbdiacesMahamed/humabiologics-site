2faf395d4223d2fd860b7c1fa9664c8d788bbbfe
AbdiacesMahamed <71865788+AbdiacesMahamed@users.noreply.github.com>
2022-07-12 13:36:34 -0700

    Create applications.js

---
+++ pages/api/products/applications.js
@@ -0,0 +1,15 @@
+// import nc from "next-connect";
+
+// const handler = nc();
+
+// handler.get(async (req, res) => {
+//   const applications = [
+//     "Human Collagen",
+//     "Human Gelatin",
+//     "Human Tissue ECM",
+//     "Human Organ ECM",
+//   ];
+//   res.send(applications);
+// });
+
+// export default handler;
