48c975f0ad425f70f97bfdf5fda547511bc07d70
AbdiacesMahamed <71865788+AbdiacesMahamed@users.noreply.github.com>
2022-07-12 19:56:17 -0700

    Update shipping.js

--- pages/shipping.js
+++ pages/shipping.js
@@ -33,7 +33,7 @@ export default function ShippingScreen() {
     setValue("postalCode", shippingAddress.postalCode);
     setValue("country", shippingAddress.country);
   }, [router, setValue, shippingAddress, userInfo]);
-
+  // build
   const submitHandler = ({ fullName, address, city, postalCode, country }) => {
     dispatch({
       type: "SAVE_SHIPPING_ADDRESS",
