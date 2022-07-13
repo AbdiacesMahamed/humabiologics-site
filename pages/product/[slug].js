7d5e5f6bc0758e3f0c112f2105ca6fec04fce306
AbdiacesMahamed <71865788+AbdiacesMahamed@users.noreply.github.com>
2022-07-12 19:03:12 -0700

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -38,6 +38,11 @@ export default function ProductScreen(props) {
   const { product, loading, error } = state;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
+    const { slug } = props;
+    const {
+      state: { cart },
+      dispatch,
+    } = useContext(Store);
     const fetchData = async () => {
       try {
         const product = await client.fetch(
