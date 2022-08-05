3b18777c04584064601e81cc9046efd3a6edfc5d
AbdiacesMahamed <71865788+AbdiacesMahamed@users.noreply.github.com>
2022-08-04 17:52:43 -0700

    Update News.js

--- pages/News.js
+++ pages/News.js
@@ -59,6 +59,13 @@ export default function Home({ posts }) {
 }
 
 export const getServerSideProps = async pageContext => {
+  const pageSlug = pageContext.query.slug;
+  
+  if (!pageSlug) {
+    return {
+      notFound: true
+    }
+  }
   const query = encodeURIComponent('*[ _type == "post" ]');
   const url = `https://wfnkzjb8.api.sanity.io/v1/data/query/production?query=${query}`;
   const result = await fetch(url).then(res => res.json());
